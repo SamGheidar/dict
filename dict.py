7247ed1a88ed10a37f3fc4d518b308351a802258
SamGheidar <sam.gheidar@gmail.com>
2022-06-09 11:42:48 +0200

    C to conn  .py

--- dict.py
+++ dict.py
@@ -7,29 +7,29 @@ conn = psycopg2.connect(
 )
 
 # read_dict: returns the list of all dictionary entries:
-# argument: C - the database connection.
-def read_dict(C):
-    cur = C.cursor()
+# argument: conn - the database connection.
+def read_dict(conn):
+    cur = conn.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
 # add_word: adds words and translation:
-# argument: C - the database connection.
-def add_word(C, word, translation):
-    cur = C.cursor()
+# argument: conn - the database connection.
+def add_word(conn, word, translation):
+    cur = conn.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
 # delete_word: deletes words and translation by asking ID:
-# argument: C - the database connection.
-def delete_word(C, ID):
-    cur = C.cursor()
+# argument: conn - the database connection.
+def delete_word(conn, ID):
+    cur = conn.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
 # save_dict: saves and close the program(stops the loop):
-# argument: C - the database connection.
-def save_dict(C):
-    cur = C.cursor()
+# argument: conn - the database connection.
+def save_dict(conn):
+    cur = conn.cursor()
     cur.execute("COMMIT;")
     cur.close()
 
